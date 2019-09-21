00164d9c5dc903cd6648a24cccf7b4c50bfa5670
dmc-account <>
2019-09-20 18:13:13 -0700

    Nothing
    Sophia Ledoux

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-
-public class Main{
+class Main{
     fun main(args: Array<String>){
         // Put code you want to run here
         // Then press the green arrow to the left to run it
+        print("Hello World")
     }
 }
